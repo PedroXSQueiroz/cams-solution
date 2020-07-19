1b364d6e78d4331137b740dc225b7177d542b4fc
Pedro <pedroxsqueiroz>
2020-07-19 18:33:20 -0300

    Adjusting prod docker

---
+++ cams-client/startup-client prod.sh
@@ -0,0 +1,22 @@
+npm install
+
+echo '----------------------------------'
+echo 'verifying rerource server is up!'
+echo '----------------------------------'
+
+until  nc -zv $RESOURCE_HOST $RESOURCE_PORT
+do
+  echo "Waiting for resource server response..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+echo '----------------------------------'
+echo 'data resource server is up'
+echo '----------------------------------'
+
+export RESOURCE_IP=$( getent hosts $RESOURCE_HOST | awk '{ print $1 }' )
+
+export HUB_IP=$( getent hosts $HUB_HOST | awk '{ print $1 }' )
+
+REACT_APP_RESOURCE_URL=$RESOURCE_PROTOCOL://$RESOURCE_IP:$RESOURCE_PORT REACT_APP_HUB_HOST=$HUB_IP REACT_APP_HUB_OUT_PORT=$HUB_OUT_PORT PORT=$FRONT_PORT lite-server -c /client/config.json
