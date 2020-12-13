806d3c1d4b315d17bd089ebd34d75f9f08726f0d
Pedro X S Queiroz <pedroxsqueiroz@gmail.com>
2020-12-12 22:27:12 -0300

    Adjusting containers

--- cams-resource-server/startup-server.sh
+++ cams-resource-server/startup-server.sh
@@ -13,6 +13,32 @@ echo '----------------------------------'
 echo 'messaging server is up'
 echo '----------------------------------'
 
+if [[ -z $DATABASE_SERVER_URL || -z $DATABASE_SERVER_PORT ]]
+then
+
+  echo '----------------------------------'
+  echo 'using local database!'
+  echo '----------------------------------'
+
+else
+  
+  echo '----------------------------------'
+  echo "requires database server, verifying database server is up $DATABASE_SERVER_URL:$DATABASE_SERVER_PORT!"
+  echo '----------------------------------'
+
+  until nc -z -v -w30 $DATABASE_SERVER_URL $DATABASE_SERVER_PORT
+  do
+    echo "Waiting for database server response..."
+    # wait for 5 seconds before check again
+    sleep 5
+  done
+
+  echo '----------------------------------'
+  echo 'database server is up!'
+  echo '----------------------------------'
+
+fi
+
 echo 'starting hub'
 
 cd /server && \
